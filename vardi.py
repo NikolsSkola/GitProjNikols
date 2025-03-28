6325721be9c88c9f6665661412cb21c72ff3b0f0
NikolsSkola <nikolsskola@gmail.com>
2025-03-28 09:46:49 +0200

    Test

--- vardi.py
+++ vardi.py
@@ -1,3 +1,15 @@
 print("Nikols")
 print("Dastins")
 print("Bruno")
+
+num1 = input("Uzraksti pirmo skaitli: ")
+darb = input("Uzraksti darbību")
+num2 = input("Uzraksti otro skaitli: ")
+
+if darb == "+":
+    print(num1 + num2)
+elif darb == "-": 
+elif darb == "*": 
+elif darb == "/": 
+else:
+    print("nav")
